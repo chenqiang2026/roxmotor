c46c79d12841833371f3eba84250993d690b53a2
chenqiang2027 <cq.charle@gmail.com>
2026-01-13 19:51:43 +0800

    Create screen_mcu_huayang.h

---
+++ 8155-QMcuScreen/screen_mcu_service/src/screen_mcu_huayang.h
@@ -0,0 +1,19 @@
+#ifndef SCREEN_MCU_HUAYANG_H
+#define SCREEN_MCU_HUAYANG_H
+#include "screen_mcu_service.h"
+#include "screen_mcu_tp.h"
+struct huayang_info {
+    uint8_t sw_ver[2];
+	uint8_t hw_ver[2];
+	uint8_t bl_ver[2];
+	uint8_t tilt_degree;
+	uint8_t tilt_stat;
+	uint8_t power_stat;
+	uint8_t open_stat;
+	uint8_t anti_pinch_stat;
+	uint8_t key_lock_stat;
+	uint8_t key_stat;
+	screen_mcu_tp_t *tp_info;
+};
+unsigned screen_mcu_huayang_init(struct screen_mcu_drv *screen_mcu_p);
+#endif
